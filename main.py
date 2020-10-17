bd84d6a670b1b52363c61f0298a3847da8d55f61
John Tikas <tikasjohn@gmail.com>
2020-10-17 09:18:05 +0300

    Created main class structure

---
+++ main.py
@@ -0,0 +1,21 @@
+class Face:
+    pass
+
+class TreeNode:
+    pass
+
+class Tree:
+    pass
+
+class PointCloud:
+    pass
+
+class Iteration:
+    pass
+
+class Settings:
+    pass
+
+class Algorithm:
+    pass
+
